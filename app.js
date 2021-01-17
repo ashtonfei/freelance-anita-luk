72c560b7b7b8f255d707f4234c0a9ac4fb3304d2
Ashton Fei <yunjia.fei@gmail.com>
2021-01-17 20:26:11 +0800

    add shipstation api

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const APP_NAME = "Label Process"
 const SN_LABEL_PASTE_HERE = "LABEL PASTE HERE"
+const SN_USPS = "USPS"
+const SN_SHIPMENTS = "SHIPMENTS(TEST)"
+
 // C > I, D > J, E > K, F > L, G > M, H > O, J > N
 const SOURCE_FOLDER_ID = "1uhi5F3FZECo7chWpdfsvjUyMagPDPgX7"
 const SN_ORDER_IMPORT = "OrderImport"
@@ -10,6 +13,9 @@ function onOpen() {
     const ui = SpreadsheetApp.getUi()
     const menu = ui.createMenu(APP_NAME)
     menu.addItem("Get label data", "getLabelData")
+    menu.addItem(`Copy to ${SN_USPS}`, 'copyToUsps')
+    menu.addSeparator()
+    menu.addItem("List Shipments(test)", "listShipments")
     menu.addToUi()
 }
 
@@ -21,9 +27,24 @@ const getLabelData = () => {
         const usedTime = Math.floor((endTime - startTime) / 1000)
         SpreadsheetApp.getActive().toast(`Label data have been refreshed. Used time ${usedTime}s.`, APP_NAME)
     } catch (e) {
-        console.log(e)
         SpreadsheetApp.getUi().alert(APP_NAME, e.message, SpreadsheetApp.getUi().ButtonSet.OK)
     }
+}
+
+const copyToUsps = () => {
+    const startTime = new Date().getTime()
+    try {
+        new App().copyToUsps()
+        const endTime = new Date().getTime()
+        const usedTime = Math.floor((endTime - startTime) / 1000)
+        SpreadsheetApp.getActive().toast(`Copied to ${SN_USPS}. Used time ${usedTime}s.`, APP_NAME)
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(APP_NAME, e.message, SpreadsheetApp.getUi().ButtonSet.OK)
+    }
+}
+
+const listShipments = () => {
+    new ShipStation().listShipments()
 }
 
 class App {
@@ -32,6 +53,7 @@ class App {
         this.ui = SpreadsheetApp.getUi()
         this.sourceFolder = this.getFolderById(SOURCE_FOLDER_ID)
         this.wsLabelPasteHere = this.ss.getSheetByName(SN_LABEL_PASTE_HERE)
+        this.wsUsps = this.ss.getSheetByName(SN_USPS)
     }
 
     getFolderById(id) {
@@ -42,9 +64,17 @@ class App {
         }
     }
 
-    isAppValid() {
+    isAppValidForGetLabelData() {
         if (!this.sourceFolder) return { valid: false, message: `Invalid source folder id: "${SOURCE_FOLDER_ID}".` }
         if (!this.wsLabelPasteHere) return { valid: false, message: `We can't find the sheet "${SN_LABEL_PASTE_HERE}".` }
+        const message = `"${APP_NAME}" APP is ready to go.`
+        this.ss.toast(message, APP_NAME, 2)
+        return { valid: true, message }
+    }
+
+    isAppValidForCopyToUsps() {
+        if (this.ss.getActiveSheet().getName() !== SN_LABEL_PASTE_HERE) return { valid: false, message: `Active sheet is not "${SN_LABEL_PASTE_HERE}."` }
+        if (!this.wsUsps) return { valid: false, message: `We can't find the sheet "${SN_USPS}".` }
         const message = `"${APP_NAME}" APP is ready to go.`
         this.ss.toast(message, APP_NAME, 2)
         return { valid: true, message }
@@ -103,7 +133,7 @@ class App {
     }
 
     getLabelData() {
-        const { valid, message } = this.isAppValid()
+        const { valid, message } = this.isAppValidForGetLabelData()
         if (!valid) {
             this.ui.alert(APP_NAME, message, this.ui.ButtonSet.OK)
             return
@@ -132,5 +162,76 @@ class App {
             }
         })
         this.wsLabelPasteHere.getRange(1, START_COLUMN_INDEX + 1, newValues.length, newValues[0].length).setValues(newValues)
+    }
+
+    copyToUsps() {
+        const { valid, message } = this.isAppValidForCopyToUsps()
+        if (!valid) {
+            this.ui.alert(APP_NAME, message, this.ui.ButtonSet.OK)
+            return
+        }
+        // copy selected rows to USPS sheet
+        const ws = this.ss.getActiveSheet()
+        const selection = ws.getSelection()
+        const ranges = selection.getActiveRangeList().getRanges()
+        const items = []
+        ranges.forEach(range => {
+            const startRow = range.getRow()
+            const values = range.getDisplayValues()
+            values.forEach((v, i) => {
+                const isRowHidden = ws.isRowHiddenByUser(startRow + i) || ws.isRowHiddenByFilter(startRow + i)
+                if (!isRowHidden) {
+                    const [order, shop, , code, , , , , buyer, addressFirstLine, addressSecondLine, city, state, country, zip] = v
+                    items.push([order, shop, code, buyer, `${addressFirstLine} ${addressSecondLine}`, city, state, country, zip])
+                }
+            })
+        })
+        const lastRow = this.wsUsps.getDataRange().getLastRow()
+        this.wsUsps.getRange(lastRow + 1, 1, items.length, items[0].length).setValues(items).activate()
+    }
+}
+
+class ShipStation {
+    constructor() {
+        const key = "API_KEY"
+        const secret = "API_SECRET"
+        this.token = Utilities.base64Encode(`${key}:${secret}`)
+        this.endpoint = "https://ssapi.shipstation.com/"
+        this.ss = SpreadsheetApp.getActive()
+        this.ui = SpreadsheetApp.getUi()
+        this.wsShipments = this.ss.getSheetByName(SN_SHIPMENTS) || this.ss.insertSheet(SN_SHIPMENTS)
+    }
+    listShipments() {
+        const url = `${this.endpoint}shipments?page=1&pageSize=50`
+        const params = {
+            method: "get",
+            headers: {
+                Authorization: `Basic ${this.token}`
+            },
+        }
+        const response = UrlFetchApp.fetch(url, params)
+        const code = response.getResponseCode()
+        if (code !== 200) return this.ui.alert(APP_NAME, `Error Code ${code}`, this.ui.ButtonSet.OK)
+        const content = JSON.parse(response.getContentText())
+        const shipments = content.shipments
+        if (!shipments) return this.ui.alert(APP_NAME, `No shipments found`, this.ui.ButtonSet.OK)
+        const values = shipments.map(shipment => {
+            return [
+                shipment.shipmentId,
+                shipment.shipmentCost,
+                shipment.shipDate,
+                shipment.trackingNumber,
+                shipment.shipTo.name,
+                shipment.shipTo.street1,
+                shipment.shipTo.street2,
+                shipment.shipTo.city,
+                shipment.shipTo.state,
+                shipment.shipTo.country,
+                shipment.shipTo.postalCode,
+            ]
+        })
+        values.unshift(["ID", "Cost", "Date", "Tracking #", "Name", "Street1", "Street 2", "City", "State", "Country", "Postal Code"])
+        this.wsShipments.clear()
+        this.wsShipments.getRange(1, 1, values.length, values[0].length).setValues(values).activate()
     }
 }
